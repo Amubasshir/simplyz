41cc28f66f5ae29f8d9118334e61b8238c958f01
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-04-25 14:54:50 +0600

    latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <section className="relative flex h-screen w-full flex-col items-center !overflow-visible rounded-md bg-neutral-950 antialiased">
-        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+      <section className="relative flex h-screen w-full flex-col items-center !overflow-visible rounded-md antialiased">
+        <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#061025_100%)]"></div>
         <div className="mt-[-100px] flex flex-col md:mt-[-50px]">
           <ContainerScroll
             titleComponent={
